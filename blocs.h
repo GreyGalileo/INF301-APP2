f7a3226d495c9a3fd30607367907550ed3516be5
PotatoCorn10 <147085239+PotatoCorn10@users.noreply.github.com>
2023-11-17 15:00:01 +0100

    Update blocs.h

--- blocs.h
+++ blocs.h
@@ -16,4 +16,14 @@ blocerr empile_bloc(sequence_d *, cellule_t *);
 
 blocerr execute_bloc(sequence_d *, cellule_t *);
 
+blocerr echange_bloc(sequence_d *pile);
+
+blocerr execute_single_bloc(sequence_d *, cellule_t *);
+
+blocerr clone_bloc(sequence_d *pile);
+
+blocerr delete_bloc(sequence_d *pile);
+
+blocerr commande_bloc(sequence_d *pile, cellule_t *p_cell);
+
 #endif
